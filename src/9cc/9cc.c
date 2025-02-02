bcd00986b37a7deeec093d9e1306225b8b8a36d7
washizu <mokamikan1027@gmail.com>
2025-02-02 22:50:41 +0900

    四則演算に対応

--- src/9cc/9cc.c
+++ src/9cc/9cc.c
@@ -5,6 +5,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+//
+// Tokenizer
+//
 
 typedef enum {
   TK_RESERVED,
@@ -22,7 +25,20 @@ struct Token {
 };
 
 char *user_input;
+
+
 Token *token;
+
+
+
+void error(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
 
 void error_at(char *loc, char *fmt, ...) {
   va_list ap;
@@ -82,7 +98,7 @@ Token *tokenize() {
       continue;
     }
 
-    if (*p == '+' || *p == '-') {
+    if (strchr("+-*/()", *p)) {
       cur = new_token(TK_RESERVED, cur, p++);
       continue;
     }
@@ -100,30 +116,136 @@ Token *tokenize() {
   return head.next;
 }
 
+//
+// Parser
+//
+
+typedef enum {
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_MUL, // *
+  ND_DIV, // /
+  ND_NUM, // 整数
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node {
+  NodeKind kind; // ノードの型
+  Node *lhs;     // 左辺
+  Node *rhs;     // 右辺
+  int val;       // kindがND_NUMの場合のみに使う
+};
+
+Node *new_node(NodeKind kind) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  return node;
+}
+
+Node *new_binary(NodeKind kind, Node *lhs, Node *rhs) {
+  Node *node = new_node(kind);
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_num(int val) {
+  Node *node = new_node(ND_NUM);
+  node->val = val;
+  return node;
+}
+
+Node *expr();
+Node *mul();
+Node *primary();
+
+
+Node *expr() {
+  Node *node = mul();
+
+  for(;;) {
+    if(consume('+')) 
+      node = new_binary(ND_ADD, node, mul());
+    else if (consume('-'))
+      node = new_binary(ND_SUB, node, mul());
+    else 
+      return node;
+  }
+}
+
+Node *mul() {
+  Node *node = primary();
+
+  for(;;) {
+    if (consume('*'))
+      node = new_binary(ND_MUL, node, primary());
+    else if (consume('/'))
+      node = new_binary(ND_DIV, node, primary());
+    else 
+      return node;
+  }
+}
+
+Node *primary() {
+  if (consume('(')) {
+    Node *node = expr();
+    expect(')');
+    return node;
+  }
+
+  return new_num(expect_number());
+}
+
+//
+// Code generator
+// 
+
+void gen(Node *node) {
+  if (node->kind == ND_NUM) {
+    printf("  mov x2, #%d\n", node->val);
+    printf("  str x2, [sp, -16]!\n");
+    return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  ldr x1, [sp], 16\n");
+  printf("  ldr x0, [sp], 16\n");
+
+  switch (node->kind) {
+    case ND_ADD:
+      printf("  add x0, x0, x1\n");
+      break;
+    case ND_SUB:
+      printf("  sub x0, x0, x1\n");
+      break;
+    case ND_MUL:
+      printf(" mul x0, x0, x1\n");
+      break;
+    case ND_DIV:
+      printf("  sdiv x0, x0, x1\n");
+      break;
+  }
+
+  printf("  str x0, [sp, -16]!\n");
+}
 
 int main(int argc, char **argv) {
-  if(argc != 2) {
-    fprintf(stderr, "引数の個数が正しくありません\n");
-    return 1;
-  }
+  if(argc != 2) 
+    error("%s: invalid number of arguments", argv[0]);
+
   user_input = argv[1];
   token = tokenize();
+  Node *node = expr();
 
   printf(".globl main\n");
   printf("main:\n");
-  
-  printf("  mov x0, #%d\n", expect_number());
 
-  while(!at_eof()) {
-    if (consume('+')) {
-      printf("  add x0, x0, #%d\n", expect_number());
-      continue;
-    }
+  gen(node);
 
-    expect('-');
-    printf("  sub x0, x0, #%d\n", expect_number());
-  }
-
+  printf("  ldr x0, [sp], 16\n");
   printf("  ret\n");
   return 0;
 }
